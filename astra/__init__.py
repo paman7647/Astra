8a39525a3804b7231b2976911e7bd4e5c065e924
paman7647 <paman7647@users.noreply.github.com>
2026-02-22 19:25:26 +0530

    chore: sync __version__ in __init__.py

--- astra/__init__.py
+++ astra/__init__.py
@@ -12,5 +12,5 @@ from .events.filters import Filters
 from .models import User, Chat, Message, ClientStatus
 from .errors import AstraError
 
-__version__ = "0.0.2b16"
+__version__ = "0.0.2b18"
 __all__ = ["Client", "Filters", "User", "Chat", "Message", "AstraError", "ClientStatus"]
