65be70dd4070e9ba46c3583b7ed33ac873d3c6b6
paman7647 <paman7647@users.noreply.github.com>
2026-03-13 18:15:50 +0530

    update v0.0.5b1

--- Astra-Files/astra/sync/__init__.py
+++
@@ -1,8 +0,0 @@
-# -----------------------------------------------------------
-# Astra - WhatsApp Client Framework
-# Licensed under the Apache License 2.0.
-# -----------------------------------------------------------
-
-"""
-Astra Sync: Background synchronization engine, scheduler, and supervisor.
-"""
